928535b52877f2029cb1934692bb174159e393ca
david <davydviniarskyi.dev@gmail.com>
2023-07-13 12:12:13 +0300

    add graphql support

---
+++ Model/Resolver/DonationResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Donmo\Roundup\Model\Resolver;
+
+use Donmo\Roundup\Logger\Logger;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Quote\Model\Quote;
+
+class DonationResolver implements ResolverInterface
+{
+    private Logger $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        /** @var Quote $quote */
+        $quote = $value['model'];
+
+        $donmoDonation = $quote->getDonmodonation();
+
+        $currency = $quote->getQuoteCurrencyCode();
+
+        return ['value' => $donmoDonation, 'currency' => $currency];
+    }
+}
