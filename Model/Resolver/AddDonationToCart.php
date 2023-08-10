5adf8ade72505f95fd8f17aa4b519b5043f4b2ec
david <davydviniarskyi.dev@gmail.com>
2023-08-10 14:07:47 +0300

    improve cart handling in graphql resolvers

--- Model/Resolver/AddDonationToCart.php
+++ Model/Resolver/AddDonationToCart.php
@@ -6,39 +6,41 @@ use Donmo\Roundup\Logger\Logger;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\Quote\Model\MaskedQuoteIdToQuoteIdInterface;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
-
 use Magento\Quote\Api\CartRepositoryInterface;
 
-class AddDonationToQuote implements ResolverInterface
+use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
+
+
+class AddDonationToCart implements ResolverInterface
 {
     private Logger $logger;
-    private MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId;
     private CartRepositoryInterface $cartRepository;
+    private GetCartForUser $getCartForUser;
     public function __construct(
         Logger $logger,
         CartRepositoryInterface $cartRepository,
-        MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId,
+        GetCartForUser $getCartForUser
     ) {
         $this->logger = $logger;
         $this->cartRepository = $cartRepository;
-        $this->maskedQuoteIdToQuoteId = $maskedQuoteIdToQuoteId;
+        $this->getCartForUser = $getCartForUser;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): array
     {
-        $maskedId = $args['cartId'];
-        $quoteId = $this->maskedQuoteIdToQuoteId->execute($maskedId);
-        $quote = $this->cartRepository->get($quoteId);
-
-        $donationAmount = $args['donationAmount'];
+        $donationAmount = $args['input']['donation_amount'];
         if (!is_numeric($donationAmount) || $donationAmount < 0) {
             throw new GraphQlInputException(__('Invalid donation'));
         }
 
-        $quote->setDonmodonation($donationAmount)->collectTotals();
-        $this->cartRepository->save($quote);
+        $maskedCartId = $args['input']['cart_id'];
+        $currentUserId = $context->getUserId();
+        $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        $cart = $this->getCartForUser->execute($maskedCartId, $currentUserId, $storeId);
+
+        $cart->setDonmodonation($donationAmount)->collectTotals();
+        $this->cartRepository->save($cart);
 
         return ['message' => 'success'];
     }
